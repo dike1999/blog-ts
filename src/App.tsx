448552ff533f011f4b6c04ebf198773ead9beb21
dike <dike@kuaishou.com>
2022-04-11 15:14:07 +0800

    feat: add alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import { FileSyncOutlined } from "@/utils/icons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <FileSyncOutlined />
         <p>
           Hello, <code>DiKe</code>
         </p>
